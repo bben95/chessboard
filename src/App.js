afe328a686ac219fa569e938c110b025dc8ccda4
bben95 <benakabp@gmail.com>
2023-01-07 20:21:01 +0530

    Final code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import knightimg from "./knightimg.jpg";
+import getPossibleKnightMoves from "./logic";
+import "./App.css";
 
 const App = () => {
   const col = [0, 1, 2, 3, 4, 5, 6, 7];
   const row = [0, 1, 2, 3, 4, 5];
   const [rows, setRow] = useState(0);
   const [cols, setCol] = useState(1);
-  const
+  const [possible,setpossible]=useState([])
+  useEffect(()=>{setpossible(getPossibleKnightMoves(rows,cols))},[rows,cols])
+ const possiblity =(i,j)=>{for (const element of possible){if(element[0]===i&& element[1]===j)return true} }
+  
 
   return (
-    <>
+
+    <div style={{position:"absolute" ,top:"5%",left:"25%"}}>
+      <h2 style={{textAlign:"center"}}>Click On blocks to find the possible positions Of Knight</h2>
       {row.map((i) => {
         return (
           <div style={{ display: "flex", flexDirection: "row" }}>
             {col.map((j) => {
               return (
                 <div
-                  style={{
+                className={possiblity(i,j)?"backgroundred":""}
+                  
+                style={{
                     border: "1px solid black",
                     width: "80px",
                     height: "80px",
-                    backgroundColor: (i + j) % 2 === 0 ? "white" : "grey",
+                
                   }}
                   onClick={() => {
                     setRow(i);
                     setCol(j);
+                    
                   }}
                 >
                   {i === rows && j === cols && (
@@ -36,7 +46,7 @@ const App = () => {
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
--- src/logic.js
+++ src/logic.js
@@ -1,27 +1,15 @@
-function findPossibleMoves(position) {
-    let column = position[0];
-    let row = position[1];
-    let possibleMoves = [];
-    let rows=[];
-    let cols=[];
-    
-    // rows and columns for all eight possible moves
-    var possibleColumns = [column - 2, column - 1, column + 1, column + 2];
-    var possibleRows = [row - 2, row - 1, row + 1, row + 2];
-    
-    // check all eight possible moves and only add to array if on the board
-    for (var i = 0; i < possibleColumns.length; i++) {
-    if (possibleColumns[i] > 0 && possibleColumns[i] < 9) {
-    for (var j = 0; j < possibleRows.length; j++) {
-    if (possibleRows[j] > 0 && possibleRows[j] < 9) {
-    cols.push(possibleColumns[i]); rows.push(  possibleRows[j]);
-    }
-    }
-    }
-    }
-    possibleMoves.push(rows);
-    possibleMoves.push(cols);
-    return possibleMoves;
-    }
-    export default findPossibleMoves;
-    
+function getPossibleKnightMoves(row, col) {
+    const possibleMoves = [
+      [row - 2, col - 1],
+      [row - 2, col + 1],
+      [row - 1, col - 2],
+      [row - 1, col + 2],
+      [row + 1, col - 2],
+      [row + 1, col + 2],
+      [row + 2, col - 1],
+      [row + 2, col + 1]
+    ];
+  
+    return possibleMoves.filter(([r, c]) => r >= 0 && r < 8 && c >= 0 && c < 8);
+  }
+  export default getPossibleKnightMoves;
